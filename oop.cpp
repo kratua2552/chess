8b771b8c5446e46a840e8cc9b60626b5ecc0cd18
iton <swittah893@gmail.com>
2025-02-05 22:39:04 +0700

    working on piece movement

--- oop.cpp
+++ oop.cpp
@@ -3,6 +3,13 @@
 #include <cstring>
 #include <cctype>
 
+#include <vector>
+#include <array>
+
+#include "hashmap.h"
+
+using namespace std;
+
 class Data {
 public:
     int log_md(int first = 0, int second = 0, int third = 0, int forth = 0, int fifth = 0) {
@@ -14,56 +21,43 @@ public:
 protected:
     struct MainData {
         int ****board;
+        hashmap_int_T ***board_data;
         int *metadata;
     };
+    
+public:
+    static MainData md;
+};
 
-    struct GameData {
-        int isGameStart;
-
-        struct GameStatistics {
-            int currentTurn, gameTime;
-        };
-        GameStatistics *gSt;
-
-        struct GameConfiguration {
-            int boardType, isUndoAllowed, timeControl;
-        };
-        GameConfiguration *gCf;
-
-        struct Profiles {
-            struct White {
-                int score, move, timeRemaining;
-            };
-            White *pW;
-
-            struct Black {
-                int score, move, timeRemaining;
-            };
-            Black *pB;
-        };
-        Profiles *pfp;
-    };
-
-    MainData md;
+Data::MainData Data::md = {
+    nullptr, nullptr
 };
 
 class Board : public Data {
 private:
     void allocate(MainData *md, int row, int col) {
-    md->board = new int***[row];
-    md->metadata = new int[3];
+        md->board = (int ****)malloc(row * sizeof(int ***));
+        md->metadata = (int *)malloc(3 * sizeof(int));
 
-    md->metadata[0] = row;
-    md->metadata[1] = col;
+        md->metadata[0] = row;
+        md->metadata[1] = col;
 
-    for (int i = 0; i < row; i++) {
-        md->board[i] = new int**[col];
+        for (int i = 0; i < row; i++) {
+            md->board[i] = (int ***)malloc(col * sizeof(int **));
 
             for (int j = 0; j < col; j++) {
-                md->board[i][j] = new int*[3];
-                md->board[i][j][0] = new int[3];
-                md->board[i][j][1] = new int[8];
-                md->board[i][j][2] = new int[4];
+                md->board[i][j] = (int **)malloc(3 * sizeof(int *));
+                md->board[i][j][0] = (int *)malloc(3 * sizeof(int));
+                md->board[i][j][2] = (int *)malloc(4 * sizeof(int));
+            }
+        }
+
+        md->board_data = new hashmap_int_T**[row];
+
+        for (int i = 0; i < row; i++) {
+            md->board_data[i] = new hashmap_int_T*[col];
+            for (int j = 0; j < col; j++) {
+                md->board_data[i][j] = new hashmap_int_T[8];
             }
         }
     }
@@ -90,13 +84,14 @@ private:
             for (int j = 0; j < md->metadata[1]; j++) {
                 int east = (md->metadata[1] - 1) - j;
                 int west = j;
-                int south = i;
-                int north = (md->metadata[0] - 1) - i;
+                int south = (md->metadata[0] - 1) - i;
+                int north = i;
 
                 int cache[8] = {north, south, west, east, mathmin(north, west), mathmin(south, east), mathmin(north, east), mathmin(south, west)};
 
                 for (int k = 0; k < 8; k++) {
-                    md->board[i][j][1][k] = cache[k];
+                    md->board_data[i][j][k].key = k;
+                    md->board_data[i][j][k].val = cache[k];
                 }
             }
         }
@@ -155,15 +150,20 @@ private:
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                delete[] md->board[i][j][0];
-                delete[] md->board[i][j][1];
-                delete[] md->board[i][j][2];
-                delete[] md->board[i][j];
+                free(md->board[i][j][0]);
+                free(md->board[i][j][2]);
+                free(md->board[i][j]);
+
+                delete[] md->board_data[i][j];
             }
-            delete[] md->board[i];
+            free(md->board[i]);
+            
+            delete[] md->board_data[i];
         }
-        delete[] md->board;
-        delete[] md->metadata;
+        free(md->board);
+        free(md->metadata);
+
+        delete[] md->board_data;
     }
 
     int boardState = -1;
@@ -172,24 +172,24 @@ public:
 
     int interface(const char* type, int row = 8, int col = 8) {
         if (strcmp(type, "delete") == 0 && boardState == -1) {
-            printf("cannot delete void");
+            printf("cannot delete void\n");
             return -1;
         }
 
         if (strcmp(type, "delete") == 0 && boardState == 0) {
             deleteboard(&md);
             boardState = -1;
-            printf("board deleted");
+            printf("board deleted\n");
             return 0;
         }
 
         if (boardState == 0) {
-            printf("cannot override board");
+            printf("cannot override board\n");
             return -1;
         }
 
         if (row < 1 || col < 1) {
-            printf("cannot initialize zero or negative board");
+            printf("cannot initialize zero or negative board\n");
             return -1;
         }
         if (strcmp(type, "default") == 0) type = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
@@ -199,16 +199,51 @@ public:
         edgecount(&md);
         insertpieces(&md, type);
         boardState = 0;
-        printf("board initalized");
+        printf("board initalized\n");
 
         return 0;
     }
+
+    ~Board() {
+        deleteboard(&md);
+        printf("board collapsed\n");
+    }
+};
+
+class Validate : public Data {
+public:
+    Validate() {
+        printf("engine constructed\n");
+    }
+
+    hashmap_int_T directions[8] = {
+        {0, md.metadata[0]}, {1, -(md.metadata[0])}, {2, -1}, {3, 1}, {4, (md.metadata[0] - 1)}, 
+        {5, -(md.metadata[0] - 1)}, {6, (md.metadata[0] + 1)}, {7, -(md.metadata[0] + 1)}
+    };
+
+    vector<array<int, 2>> qrb(int row, int col) {
+        int stDirIndx = (md.board[row][col][2][1] == 2) ? 0 : 0;
+        int enDirIndx = (md.board[row][col][2][1] == 4) ? 8 : 8;
+        int enemColor = (md.board[row][col][2][2] == 8) ? 16 : 8;
+        vector<array<int, 2>> vcache;
+
+        for (int dirIndx = stDirIndx; dirIndx < enDirIndx; dirIndx++) {
+            int enemCount = 0;
+
+            for (int i = 0; i < hashmap_int_pairs(directions, md.board_data[row][col], 8, dirIndx); i++) {
+                
+            }
+        }
+
+        return vcache;
+    }
+
 };
 
 int main() {
     Board board;
-    board.interface("default", 8, 8);
     
+    board.interface("default", 8, 8);
     printf("size: %d(row) x %d(col)\n\n", board.log_md(1, 0), board.log_md(1, 1));
 
     for (int i = 0; i < board.log_md(1, 0); i++) {
@@ -217,4 +252,14 @@ int main() {
         }
         printf("\n\n");
     }
+
+    Validate eng;
+    eng.qrb(7, 2);
+    printf("%d north, south, west, east, mathmin(north, west), mathmin(south, east), mathmin(north, east), mathmin(south, west)\n", board.md.board[7][2][0][0]);
+
+    for (int i = 0;  i < 8; i++) {
+        printf("%d, ", hashmap_int_retrieve(board.md.board_data[7][2], 8, i));
+    }
+    
+    return 0;
 }
