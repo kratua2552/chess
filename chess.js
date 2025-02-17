534b6935a1c4ce6f29e49fa94f10d946aa0dc5a3
Chanamon Saenchai <chanamon_saen@cmu.ac.th>
2025-02-17 20:30:22 +0700

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,46 @@
+// piece image
+function insertImage() {
+
+    document.querySelectorAll('.box').forEach(image => {
+
+        if (image.innerText.length !== 0) {
+            if (image.innerText == 'white_pawn' || image.innerText == 'black_pawn') {
+                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+
+            }
+
+            else {
+
+                image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.png" alt="">`
+                image.style.cursor = 'pointer'
+            }
+        }
+    })
+}
+insertImage()
+
+
+// square colors
+
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(240, 201, 150)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(100, 75, 43)'
+        }
+    })
+}
+coloring()
